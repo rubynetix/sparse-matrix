c0ef8a2501c2ad0db891db4b01d5b04ff1f23e2f
Ryan Furrer <rfurrer@ualberta.ca>
2019-01-28 15:37:58 -0700

    Feature/travis (#2)
    * Travis file - test job created
    * Linting - rubocop -travis
    * Sparse matrix Test class structure

---
+++ test/sparse_matrix_test.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+require_relative '../lib/sparse_matrix'
+
+class SparseMatrixTest < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_nothing
+  end
+end
