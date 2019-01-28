4b2f6211cd46db42dc54743731d89477333c031e
Ryan Furrer <rfurrer@ualberta.ca>
2019-01-28 15:53:05 -0700

    move ruby files to lib

--- lib/sparse_matrix.rb
+++ lib/sparse_matrix.rb
@@ -1,3 +1,175 @@
-# Abstract factory
 class SparseMatrix
+
+  def self.zero(rows, cols)
+    raise "Not implemented"
+  end
+
+  def self.identity(rows, cols)
+      raise "Not implemented"
+  end
+
+  def rows
+    raise "Not implemented"
+  end
+
+  def cols
+    raise "Not implemented"
+  end
+
+  def det
+    raise "Not implemented"
+  end
+
+  def resize(rows, cols)
+    raise "Not implemented"
+  end
+
+  def set_zero
+    raise "Not implemented"
+  end
+
+  def set_identity
+    raise "Not implemented"
+  end
+
+  def at(row, col)
+    raise "Not implemented"
+  end
+
+  def sum
+    raise "Not implemented"
+  end
+
+  def +(o)
+    raise "Not implemented"
+  end
+
+  def -(o)
+    raise "Not implemented"
+  end
+
+  def *(o)
+    raise "Not implemented"
+  end
+
+  def **(o)
+    raise "Not implemented"
+  end
+
+  def ==(o)
+    raise "Not implemented"
+  end
+
+  def to_s
+    raise "Not implemented"
+  end
+
+  def insert(row, col, val)
+    raise "Not implemented"
+  end
+
+  def diagonal
+    raise "Not implemented"
+  end
+
+  def tridiagonal
+    raise "Not implemented"
+  end
+
+  def cofactor(row, col)
+    raise "Not implemented"
+  end
+
+  def adjoint
+    raise "Not implemented"
+  end
+
+  def inverse
+    raise "Not implemented"
+  end
+
+  def rank
+    raise "Not implemented"
+  end
+
+  def transpose
+    raise "Not implemented"
+  end
+
+  def rref
+    raise "Not implemented"
+  end
+
+  def trace
+    raise "Not implemented"
+  end
+
+  def null?
+    raise "Not implemented"
+  end
+
+  def identity?
+    raise "Not implemented"
+  end
+
+  def square?
+    raise "Not implemented"
+  end
+
+  def positive?
+    raise "Not implemented"
+  end
+
+  def invertible?
+    raise "Not implemented"
+  end
+
+  def symmetric?
+    raise "Not implemented"
+  end
+
+  def traceable?
+    raise "Not implemented"
+  end
+
+  def orthogonal?
+    raise "Not implemented"
+  end
+
+  def diagonal?
+    raise "Not implemented"
+  end
+
+  def lower_triangular?
+    raise "Not implemented"
+  end
+
+  def upper_triangular?
+    raise "Not implemented"
+  end
+
+  def lower_hessenberg?
+    raise "Not implemented"
+  end
+
+  def upper_hessenberg?
+    raise "Not implemented"
+  end
+
+private
+  def plus_matrix(o)
+    raise "Not implemented"
+  end
+
+  def plus_scalar(o)
+    raise "Not implemented"
+  end
+
+  def mul_matrix(x)
+    raise "Not implemented"
+  end
+
+  def mul_scalar(x)
+    raise "Not implemented"
+  end
 end
