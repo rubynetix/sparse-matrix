a2465d12a4460d080902ab51a934df1bc3aa4450
LionKingSimba <ttran1@ualberta.ca>
2019-01-31 16:33:48 -0700

    Add * and ** tests

---
+++ test/sparse_matrix_test_tim.rb
@@ -0,0 +1,76 @@
+require 'test/unit'
+require_relative '../lib/sparse_matrix'
+
+module TestUtil
+  def rand_range(l, h, size)
+    i = 0
+    a = []
+    while i < size
+      a.push(rand(l..h))
+    end
+    a
+  end
+
+  # TODO: Implement function
+  def rand_matrix(r, c)
+    # return r x c matrix with random values.
+  end
+end
+
+class SparseMatrixTest < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_nothing
+  end
+
+  def tst_*
+    r = rand(0..10000)
+    c = rand(1..10000)
+    m = rand_matrix(r, c)
+    TestUtil::rand_range(1, 1000, 20).each do |mult|
+      # Preconditions
+      begin
+
+      end
+
+      new_m = m.*(mult)
+
+      # Postconditions
+      begin
+        (0..r).each do |i|
+          (0..c).each do |j|
+            assert_equal(m.at(i, j) * mult, new_m.at(i, j))
+          end
+        end
+      end
+    end
+  end
+
+  def tst_**
+    r = rand(0..10000)
+    c = rand(1..10000)
+    m = rand_matrix(r, c)
+    TestUtil::rand_range(1, 1000, 20).each do |exp|
+      # Preconditions
+      begin
+
+      end
+
+      new_m = m.**(exp)
+
+      # Postconditions
+      begin
+        expected = m
+        (0..exp).each do |i|
+          expected = expected.*(m)
+        assert_equal(expected, new_m)
+      end
+    end
+  end
+
+end
