971258b8f275b620fbb51b212a9a9e5908aa3142
LionKingSimba <ttran1@ualberta.ca>
2019-02-02 14:38:48 -0700

    Refactor upper and lower triangular tests and add hessenberg tests

--- test/sparse_matrix_test_tim.rb
+++ test/sparse_matrix_test_tim.rb
@@ -78,68 +78,158 @@ class SparseMatrixTest < Test::Unit::TestCase
     end
   end
 
-  def tst_lower_triangular
-    for i in 0..5
+  def tst_lower_triangular_nonsquare
+    r = 0
+    c = 0
+    while r != c
       r = rand(0..10000)
-      c = rand(1..10000)
+      c = rand(0..10000)
       m = rand_matrix(r, c)
+    end
 
       # No Preconditions
 
-      expected = sparse_to_matrix(s).lower_triangular?
-
       # Postconditions
       begin
-        assert_equal(expected, m.lower_triangular?)
+        assert_equal(sparse_to_matrix(m).lower_triangular?, m.lower_triangular?)
       end
     end
+  end
 
-    # test with 15 matrices that are guaranteed to be square.
-    for i in 0..15
+  def tst_lower_triangular_square
+    for i in 0..20
       rc = rand(0..10000)
       m = rand_matrix(rc, rc)
 
       # No Preconditions
 
-      expected = sparse_to_matrix(s).lower_triangular?
-
       # Postconditions
       begin
-        assert_equal(expected, m.lower_triangular?)
+        assert_equal(sparse_to_matrix(m).lower_triangular?, m.lower_triangular?)
       end
     end
   end
 
-  def tst_upper_triangular
-    for i in 0..5
+  def tst_upper_triangular_nonsquare
+    r = 0
+    c = 0
+    while r != c
       r = rand(0..10000)
-      c = rand(1..10000)
+      c = rand(0..10000)
       m = rand_matrix(r, c)
-
-      # No Preconditions
-
-      expected = sparse_to_matrix(s).upper_triangular?
-
-      # Postconditions
-      begin
-        assert_equal(expected, m.upper_triangular?)
-      end
     end
 
-    # test with 15 matrices that are guaranteed to be square.
-    for i in 0..15
+    # No Preconditions
+
+    # Postconditions
+    begin
+      assert_equal(sparse_to_matrix(m).upper_triangular?, m.upper_triangular?)
+    end
+  end
+
+  def tst_upper_triangular_square
+    for i in 0..20
       rc = rand(0..10000)
       m = rand_matrix(rc, rc)
 
       # No Preconditions
 
-      expected = sparse_to_matrix(s).upper_triangular?
-
       # Postconditions
       begin
-        assert_equal(expected, m.upper_triangular?)
+        assert_equal(sparse_to_matrix(m).upper_triangular?, m.upper_triangular?)
       end
     end
   end
 
+  def check_lower_hessenberg(m)
+    # algorithm to test if matrix m is lower_hessenberg
+    if !m.square?
+      assert(!m.lower_hessenberg?)
+    else
+      for y in 0...m.rows
+        for x in 0...m.cols
+          if x > y + 1
+            assert_equal(0, m.at(x, y))
+          end
+        end
+      end
+    end
+  end
+
+  def tst_lower_hessenberg_nonsquare
+    # tests lower_hessenberg? with a nonsquare matrix
+    r = 0
+    c = 0
+    while r != c
+      r = rand(0..10000)
+      c = rand(0..10000)
+      m = rand_matrix(r, c)
+    end
+      # No Preconditions
+
+      # Postconditions
+    begin
+      check_lower_hessenberg(m)
+    end
+  end
+
+  def tst_lower_hessenberg_square
+    # tests lower_hessenberg with a square matrix
+    for i in 0..20
+      rc = rand(0..10000)
+      m = rand_matrix(rc, cc)
+      # No Preconditions
+
+      # Postconditions
+      begin
+        check_lower_hessenberg(m)
+      end
+    end
+  end
+
+  def check_upper_hessenberg(m)
+    # algorithm to test if matrix m is upper_hessenberg
+    if !m.square?
+      assert(!m.upper_hessenberg?)
+    else
+      for y in 0...m.rows
+        for x in 0...m.cols
+          if y > x + 1
+            assert_equal(0, m.at(x, y))
+          end
+        end
+      end
+    end
+  end
+
+  def tst_upper_hessenberg_nonsquare
+    # tests upper_hessenberg? with a nonsquare matrix
+    r = 0
+    c = 0
+    while r != c
+      r = rand(0..10000)
+      c = rand(0..10000)
+      m = rand_matrix(r, c)
+    end
+      # No Preconditions
+
+      # Postconditions
+    begin
+      check_upper_hessenberg(m)
+    end
+  end
+
+  def tst_upper_hessenberg_square
+    # tests upper_hessenberg with a square matrix
+    for i in 0..20
+      rc = rand(0..10000)
+      m = rand_matrix(rc, cc)
+      # No Preconditions
+
+      # Postconditions
+      begin
+        check_upper_hessenberg(m)
+      end
+    end
+  end
 end
