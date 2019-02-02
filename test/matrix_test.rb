7bdfde69686fe280cde61165eb07b9e04c389a3d
Fraser Bulbuc <fraserbulbuc@gmail.com>
2019-02-02 16:12:04 -0700

    More API contracts (#12)
    * Trace, inverse, square?, symmetric?, and positive? contracts
    
    * Reorganized preconditions
    
    * Optional named args for creating random matrices

--- test/matrix_test.rb
+++ test/matrix_test.rb
@@ -2,8 +2,13 @@ require 'test/unit'
 require_relative '../lib/sparse_matrix'
 
 module MatrixTestUtil
-  def rand_matrix(rows, cols, low, high)
+
+  def rand_sparse(rows: rand(1..1000), cols: rand(1..1000), range: (-1000..1000))
     # TODO: Implementation
+  end
+
+  def rand_square_sparse(size: 1000, range: -1000..1000)
+    rand_sparse(rows: size, cols: size, range: range)
   end
 end
 
@@ -21,10 +26,11 @@ class MatrixTest < Test::Unit::TestCase
   end
 
   def tst_set_zero
+    m = MatrixTestUtil::rand_sparse()
+
     # Preconditions
     # N/A
 
-    m = MatrixTestUtil::rand_matrix(rand(1..@@MAX_ROWS), rand(1..@@MAX_COLS), @@MIN_VAL, @@MAX_VAL)
     m.set_zero
 
     # Postconditions
@@ -38,11 +44,12 @@ class MatrixTest < Test::Unit::TestCase
   end
 
   def tst_set_identity
+    m = MatrixTestUtil::rand_sparse()
+
     # Preconditions
     # N/A
 
-    m = MatrixTestUtil::rand_matrix(rand(1..@@MAX_ROWS), rand(1..@@MAX_COLS), @@MIN_VAL, @@MAX_VAL)
-    m.set_zero
+    m.set_identity
 
     # Postconditions
     begin
@@ -59,10 +66,11 @@ class MatrixTest < Test::Unit::TestCase
   end
 
   def tst_clone
+    m1 = MatrixTestUtil::rand_sparse()
+
     # Preconditions
     # N/A
 
-    m1 = MatrixTestUtil::rand_matrix(rand(1..@@MAX_ROWS), rand(1..@@MAX_COLS), @@MIN_VAL, @@MAX_VAL)
     m2 = m1.clone
 
     # Postconditions
@@ -76,10 +84,11 @@ class MatrixTest < Test::Unit::TestCase
   end
 
   def tst_sum
+    m = MatrixTestUtil::rand_sparse()
+
     # Preconditions
     # N/A
 
-    m = MatrixTestUtil::rand_matrix(rand(1..@@MAX_ROWS), rand(1..@@MAX_COLS), @@MIN_VAL, @@MAX_VAL)
     sum = m.sum
 
     # Postconditions
@@ -96,45 +105,223 @@ class MatrixTest < Test::Unit::TestCase
     end
   end
 
-  def tst_add
+  def tst_add_scalar
+    m1 = MatrixTestUtil::rand_sparse()
+    num = rand(@@MIN_VAL..@@MAX_VAL)
+
     # Preconditions
     # N/A
 
-    m1 = MatrixTestUtil::rand_matrix(rand(1..@@MAX_ROWS), rand(1..@@MAX_COLS), @@MIN_VAL, @@MAX_VAL)
-    m2 = m1.clone
-    num = rand(@@MIN_VAL..@@MAX_VAL)
-    m + num
+    m2 = m1 + num
 
     # Postconditions
     begin
-      assert_equal(m1.sum, m2.sum + num*m2.nnz)
+      assert_equal(m1.sum + num * m1.nnz, m2.sum)
 
       (0..m1.rows).each do |r|
         (0..m1.cols).each do |c|
-          assert_equal(m1.at(r, c), m2.at(r, c) + num)
+          assert_equal(m1.at(r, c) + num, m2.at(r, c))
         end
       end
     end
   end
 
-  def tst_subtract
+  def tst_subtract_scalar
+    m1 = MatrixTestUtil::rand_sparse()
+    num = rand(@@MIN_VAL..@@MAX_VAL)
+
     # Preconditions
     # N/A
 
-    m1 = MatrixTestUtil::rand_matrix(rand(1..@@MAX_ROWS), rand(1..@@MAX_COLS), @@MIN_VAL, @@MAX_VAL)
-    m2 = m1.clone
-    num = rand(@@MIN_VAL..@@MAX_VAL)
-    m1 - num
+    m2 = m1 - num
 
     # Postconditions
     begin
-      assert_equal(m1.sum, m2.sum - num*m2.nnz)
+      assert_equal(m1.sum - num * m1.nnz, m2.sum)
 
       (0..m1.rows).each do |r|
         (0..m1.cols).each do |c|
-          assert_equal(m1.at(r, c), m2.at(r, c) - num)
+          assert_equal(m1.at(r, c) - num, m2.at(r, c))
         end
       end
+    end
+  end
+
+  def tst_add_matrix
+    r = rand(1..@@MAX_ROWS)
+    c = rand(1..@@MAX_COLS)
+    m1 = MatrixTestUtil::rand_sparse(rows: r, cols: c)
+    m2 = MatrixTestUtil::rand_sparse(rows: r, cols: c)
+
+    #Preconditions
+    begin
+      assert_equal(m1.rows, m2.rows)
+      assert_equal(m1.cols, m2.cols)
+    end
+
+    m3 = m1 + m2
+
+    # Postconditions
+    begin
+      assert_equal(m1.sum + m2.sum, m3.sum)
+
+      if m1.traceable?
+        assert_equal(m1.trace + m2.trace, m3.trace)
+      end
+
+      assert_equal(m1, m3 - m2)
+
+      (0..m1.rows).each do |r|
+        (0..m1.cols).each do |c|
+          assert_equal(m1.at(r, c) + m2.at(r, c), m3.at(r, c))
+        end
+      end
+    end
+  end
+
+  def tst_subtract_matrix
+    r = rand(1..@@MAX_ROWS)
+    c = rand(1..@@MAX_COLS)
+    m1 = MatrixTestUtil::rand_sparse(rows: r, cols: c)
+    m2 = MatrixTestUtil::rand_sparse(rows: r, cols: c)
+
+    #Preconditions
+    begin
+      assert_equal(m1.rows, m2.rows)
+      assert_equal(m1.cols, m2.cols)
+    end
+
+    m3 = m1 - m2
+
+    # Postcondition
+    begin
+      assert_equal(m1.sum - m2.sum, m3.sum)
+
+      if m1.traceable?
+        assert_equal(m1.trace - m2.trace, m3.trace)
+      end
+
+      assert_equal(m1, m3 + m2)
+
+      (0..m1.rows).each do |r|
+        (0..m1.cols).each do |c|
+          assert_equal(m1.at(r, c) - m2.at(r, c), m3.at(r, c))
+        end
+      end
+    end
+  end
+
+  def tst_invertible
+    m = MatrixTestUtil::rand_sparse()
+
+    # Preconditions
+    # N/A
+
+    inv = m.invertible?
+
+    # Postconditions
+    begin
+      assert_equal(m.square? && m.det != 0, inv)
+    end
+  end
+
+  def tst_inverse
+    m = MatrixTestUtil::rand_square_sparse()
+
+    # Preconditions
+    begin
+      m.invertible?
+    end
+
+    inv = m.inverse
+
+    # Postconditions
+    begin
+      assert_equal(m * inv, SparseMatrix::identity(m.rows))
+    end
+  end
+
+  def tst_positive
+    pos_m = MatrixTestUtil::rand_sparse(range: 0..@@MAX_VAL)
+    neg_m = MatrixTestUtil::rand_sparse(range: @@MIN_VAL..-1)
+
+    # Preconditions
+    # N/A
+
+    pos = pos_m.positive?
+    neg = neg_m.positive?
+
+    # Postconditions
+    begin
+      assert_true(pos)
+      assert_false(neg)
+    end
+  end
+
+
+  def tst_square
+    m = MatrixTestUtil::rand_sparse()
+
+    # Preconditions
+    # N/A
+
+    sq = m.square?
+
+    # Postconditions
+    begin
+      assert_equal(m.rows == m.cols, sq)
+    end
+  end
+
+  def tst_symmetric
+    m = MatrixTestUtil::rand_sparse()
+
+    # Preconditions
+    # N/A
+
+    sym = m.symmetric?
+
+    # Postconditions
+    begin
+      assert_equal(m == m.transpose, sym)
+    end
+  end
+
+  def tst_traceable
+    m = MatrixTestUtil::rand_sparse()
+
+    # Preconditions
+    # N/A
+
+    tr = m.traceable?
+
+    # Postconditions
+    begin
+      assert_equal(m.square?, tr)
+    end
+  end
+
+  def tst_trace
+    m = MatrixTestUtil::rand_sparse()
+
+    # Preconditions
+    begin
+      m.traceable?
+    end
+
+    tr = m.trace
+
+    # Postconditions
+    begin
+      assert_equal(m.diagonal.trace, tr)
+      assert_equal(m.diagonal.sum, tr)
+
+      trace = 0
+      (0..m.rows).each do |r|
+        trace += m.at(r, r)
+      end
+
+      assert_equal(trace, tr)
     end
   end
 end
