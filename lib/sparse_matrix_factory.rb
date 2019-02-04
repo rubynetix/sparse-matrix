37c9e2f7665d3fe805227c8aadfd0adf6355a2cb
Ryan Furrer <rfurrer@ualberta.ca>
2019-02-04 13:34:45 -0700

    Changes

--- lib/sparse_matrix_factory.rb
+++ lib/sparse_matrix_factory.rb
@@ -1,35 +1,5 @@
 # Compressed Sparse Row (CSR) sparse matrix factory
 class SparseMatrixFactory
 
-  def initialize(*args)
-
-  end
-
-  def rows
-    raise "Not implemented"
-  end
-
-  def cols
-    raise "Not implemented"
-  end
-
-  def nnz
-    raise "Not implemented"
-  end
-
-  def resize(rows, cols)
-    raise "Not implemented"
-  end
-
-  def set_zero
-    raise "Not implemented"
-  end
-
-  def set_identity
-    raise "Not implemented"
-  end
-
-  def at(row, col)
-    raise "Not implemented"
-  end
+  def initialize(*args); end
 end
