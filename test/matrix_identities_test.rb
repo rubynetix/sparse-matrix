980c8abf759be70ae4d8a435d90eca19ca6586ed
Fraser <fbulbuc@ualberta.ca>
2019-02-09 14:35:26 -0700

    Multiplication identities

--- test/matrix_identities_test.rb
+++ test/matrix_identities_test.rb
@@ -5,7 +5,7 @@ require_relative './matrix_test_util'
 class MatrixIdentitiesTest < Test::Unit::TestCase
   include MatrixTestUtil
 
-  TEST_ITER = 100
+  TEST_ITER = 10
   MAX_ROWS = 100
   MAX_COLS = 100
   MIN_VAL = -100
@@ -14,7 +14,7 @@ class MatrixIdentitiesTest < Test::Unit::TestCase
   def test_addition_identities
     # Verify addition identities hold
     (0..TEST_ITER).each do
-      a = rand_square_sparse(size: 5, range: (0..9))
+      a = rand_square_sparse
       b = rand_sparse(rows: a.rows, cols: a.cols)
       c = rand_sparse(rows: a.rows, cols: a.cols)
       zero = SparseMatrix.zero(a.rows, a.cols)
@@ -37,5 +37,29 @@ class MatrixIdentitiesTest < Test::Unit::TestCase
       # Associativity
       assert_equal((a + b) + c, a + (b + c), "Associativity does not hold")
     end
+  end
+
+  def test_multiplication_identities
+    # TODO: Uncomment a_inv and uncomment all associated identities once inverse is implemented
+    a = rand_square_sparse(size: 5, range: (0..9))
+    # a_inv = a.inverse
+    b = rand_sparse(rows: a.rows, cols: a.cols)
+    c = rand_sparse(rows: a.rows, cols: a.cols)
+    i = SparseMatrix.identity(a.rows)
+
+    # Multiplicative identity
+    assert_equal(a * i, a, "Multiplicative identity does not hold.")
+    assert_equal(i * a, a, "Multiplicative identity does not hold.")
+
+    # Multiplicative inverse identities
+    # TODO: Activate with inverse implementation
+    # assert_equal(a * a_inv, i, "Multiplicative inverse identity does not hold.")
+    # assert_equal(a_inv.inverse, a, "Multiplicative inverse identity does not hold.")
+    # assert_equal(i.inverse, i, "Multiplicative inverse identity does not hold.")
+
+    # Grouping identities
+    assert_equal(a * (b * c), (a * b) * c, "Grouping identity does not hold.")
+    assert_equal(a * (b * -1), (a * b) * -1, "Grouping identity does not hold.")
+    assert_equal((a * b) * -1, (a * -1) * b, "Grouping identity does not hold.")
   end
 end
