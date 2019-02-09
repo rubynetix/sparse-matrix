454a54a2c46ae514e8beb8794be7859f92f072ab
Ryan Furrer <rfurrer@ualberta.ca>
2019-02-08 19:39:49 -0700

    imp/transpose

--- lib/sparse_matrix.rb
+++ lib/sparse_matrix.rb
@@ -161,7 +161,13 @@ class SparseMatrix
   end
 
   def transpose
-    raise 'Not implemented'
+    m = new @rows, @cols
+    iter = iterator
+    while iter.has_next?
+      row, col, val = iter.next
+      m.insert col, row, val
+    end
+    m
   end
 
   def trace
