fa7f89e754e3b8752362cc84bc4a911c2c4713e2
Fraser <fbulbuc@ualberta.ca>
2019-02-09 21:43:41 -0700

    Tridiag at, put, resize

--- lib/tri_diagonal_iterator.rb
+++ lib/tri_diagonal_iterator.rb
@@ -1,19 +1,20 @@
 class TriDiagonalIterator
   def initialize(low_diag, main_diag, up_diag)
-    @diags = [low_diag, main_diag, up_diag]
+    @diags = [main_diag, up_diag, low_diag]
     @idx = 0
   end
 
   def has_next?
-    @idx < @diags[1].length
+    @idx < (@diags[0].length + @diags[1].length + @diags[2].length)
   end
 
   def next
-    block, offset = @idx % 3, @idx / 3
+    diag, offset = @idx % 3, @idx / 3
 
-    val = @diags[block][offset]
-    row = @idx + row_offset(@idx)
-    col = @idx + col_offset(@idx)
+    val = @diags[diag][offset]
+    row = offset + row_offset(@idx)
+    col = offset + col_offset(@idx)
+    # puts "idx #{@idx} --- \n diag #{diag}, off #{offset}, roff #{row_offset(@idx)}, coff #{col_offset(@idx)}"
     @idx += 1
 
     [row, col, val]
@@ -27,11 +28,11 @@ class TriDiagonalIterator
 
   def row_offset(idx)
     # Element from lower diag is 1 row below.
-    idx % 2 == 0 ? 1 : 0
+    idx % 3 == 2 ? 1 : 0
   end
 
   def col_offset(idx)
     # Element from upper diag is 1 column ahead.
-    idx % 2 == 1 ? 1 : 0
+    idx % 3 == 1 ? 1 : 0
   end
 end
