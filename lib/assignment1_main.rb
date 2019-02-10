506f61563370aece41ebbece63b9f6e8afa28de9
Fraser <fbulbuc@ualberta.ca>
2019-02-10 16:07:21 -0700

    Aligned example comments

--- lib/assignment1_main.rb
+++ lib/assignment1_main.rb
@@ -14,12 +14,12 @@ require_relative 'tridiagonal_matrix_factory'
 sparse_fact = SparseMatrixFactory.new
 tri_fact = TriDiagonalMatrixFactory.new
 
-s1 = sparse_fact.new(3, 3)                                # Create a 3x3 matrix filled with zeroes
-s2 = sparse_fact.new(3, 3, 1)                             # Create a 3x3 matrix filled with ones
-s3 = sparse_fact.identity(3)                                 # Create a 3x3 identity matrix
-s4 = sparse_fact.from_array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) # Create a 3x3 matrix from arrays
+s1 = sparse_fact.new(3, 3)                                         # Create a 3x3 matrix filled with zeroes
+s2 = sparse_fact.new(3, 3, 1)                                      # Create a 3x3 matrix filled with ones
+s3 = sparse_fact.identity(3)                                       # Create a 3x3 identity matrix
+s4 = sparse_fact.from_array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])     # Create a 3x3 matrix from arrays
 
-t1 = tri_fact.new(3, 3)                                   # Create a 3x3 tridiagonal matrix filled with zeroes
-t2 = tri_fact.new(3, 3, 1)                                # Create a 3x3 tridiagonal matrix filled with ones
-t3 = tri_fact.identity(3)                                    # Create a 3x3 tridiagonal, identity matrix
-t4 = tri_fact.from_diags([[1, 2], [3, 4, 5], [6, 7]])          # Create a 3x3 tridiagonal matrix from specified diagonals
+t1 = tri_fact.new(3, 3)                                            # Create a 3x3 tridiagonal matrix filled with zeroes
+t2 = tri_fact.new(3, 3, 1)                                         # Create a 3x3 tridiagonal matrix filled with ones
+t3 = tri_fact.identity(3)                                          # Create a 3x3 tridiagonal, identity matrix
+t4 = tri_fact.from_diags([[1, 2], [3, 4, 5], [6, 7]])              # Create a 3x3 tridiagonal matrix from specified diagonals
